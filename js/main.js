2f62f94bc6d6b464db8c39a9fe9d3c8c51098900
TechDev161 <devcalvinlow@gmail.com>
2024-06-30 19:51:23 -0700

    Updated html to obtain value for userChoice; Added score to JS; Added score to HTML;

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 let displayResult = document.querySelector('#result')
+let displayUserChoice = document.querySelector('#userChoice')
 let displayCompChoice = document.querySelector('#compChoice')
+
 let displayScore = document.querySelector('#score')
+let buttons = document.querySelectorAll('button')
+
+
+let buttonRock = document.querySelector('#Rock')
+let buttonPaper = document.querySelector('#Paper')
+let buttonScissor = document.querySelector('#Scissor')
+
+
 
 let userPoints = 0
 let compPoints = 0
 
-function rockPaperScissor () {
+function determineCompChoice () {
     let computerChoice = Math.random()
     if (computerChoice < 0.33) {
         return "Rock"
@@ -16,31 +26,26 @@ function rockPaperScissor () {
     }
 }
 
-function playGame() {
-    let compChoice = rockPaperScissor()
+
+function determineResult(userChoice) {
+    let compChoice = determineCompChoice()
+    displayUserChoice.innerText =  userChoice
     displayCompChoice.innerText = compChoice
-
-
-   // let userChoice = document.querySelector('button').value
-
 
     if (userChoice == "Rock" && compChoice == "Scissor" || userChoice == "Paper" && compChoice == "Rock" || userChoice == "Scissor" && compChoice == "Paper"){
         displayResult.innerText = "You Win"
+        userPoints++
 
     } else if (userChoice == "Rock" && compChoice == "Paper" || userChoice == "Paper" && compChoice == "Scissor" || userChoice == "Scissor" && compChoice == "Rock"){
        displayResult.innerText = "Computer Wins"
-
+       compPoints++
 
 } else {
         displayResult.innerText = "You both tie!"
 
 }
 
+document.querySelector('#score').innerText = `${userPoints} : ${compPoints}`
 }
 
-
-
-document.querySelector('#Rock').addEventListener('click',playGame)
-document.querySelector('#Paper').addEventListener('click',playGame)
-document.querySelector('#Scissor').addEventListener('click',playGame)
 
